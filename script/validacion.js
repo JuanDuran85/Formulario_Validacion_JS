e791508b48d7dccfca8e9337e1f40b68de068a08
Ana <misushima@gmail.com>
2020-02-18 15:06:13 -0300

    Se agrega funcion de validacion de fecha

--- script/validacion.js
+++ script/validacion.js
@@ -4,9 +4,16 @@ window.onload = function()
     let submit = document.getElementById("enviar");
     submit.addEventListener("click", function(event)
     {
+        date.dateFormat = new Date
+        (
+            document.getElementById("ano").value,
+            document.getElementById("mes").value - 1,
+            document.getElementById("dia").value
+        );
         event.preventDefault();
         valCampo(name);
         valCampo(phone);
+        valDate(date);
     });
     let name = 
     {
@@ -22,10 +29,18 @@ window.onload = function()
         regex: /[\d\s]+/,
         element: 'telefono'
     }
+    let date = 
+    {
+        day: document.getElementById("dia"),
+        month: document.getElementById("mes"),
+        year: document.getElementById("ano"),
+        maxLength: 7,
+        regex: /[\d\s]+/,
+        element: 'fecha de nacimiento' 
+    }
 
     let valCampo = function(objeto)
     {
-        console.log(objeto)
         if(objeto.dom.value == "")
         {
             console.log("el "+objeto.element+" está vacio");
@@ -38,6 +53,21 @@ window.onload = function()
         {
             console.log("el "+objeto.element+" supera los "+objeto.maxLength+" caracteres.");
         }
+    }
+
+    let valDate = function(date)
+    {
+        if(date.day.value == "" || date.month.value == "" || date.year.value == "")
+        {
+            console.log("la "+date.element+" está vacia");
+        }
+        else if ((date.dateFormat.getUTCMonth() != date.month.value - 1) || (
+            date.dateFormat.getUTCDate() != date.day.value) || (
+            date.dateFormat.getUTCFullYear() != date.year.value))
+        {
+            console.log("la "+date.element+" no existe");
+        }
+
     }
 
    
